3cb86a1a1d0f56d530ed531fefa82d29b21aa7c9
Michael Pope <michael@dtcorp.com.au>
2009-03-12 10:01:31 +1100

    Added AJAX calls for ctrl & fnkey shortcuts

--- lib/shortcuts_helper.rb
+++ lib/shortcuts_helper.rb
@@ -22,6 +22,12 @@ module ShortcutsHelper
     shortcut_ctrl_function key, "document.location.href='#{url}';"
   end
 
+  # Register a keyboard shortcut for a given url. When the shortcut key is pressed,
+  # the url is called remotely via an Ajax call.
+  def remote_shortcut_ctrl key, url, options={:method=>:get}
+   shortcut_ctrl_function key, remote_function(options.merge(:url=>url))
+  end
+
   # Used to jump to locations using Ctrl + <char>
   def shortcut_ctrl_function key, function
     javascript_tag("Shortcuts.register_shortcut_ctrl('#{key}', function() { #{ function } });")
@@ -30,6 +36,12 @@ module ShortcutsHelper
   # Used to jump to locations using Fnkey
   def shortcut_fnkey key, url
     shortcut_fnkey_function key, "document.location.href='#{url}';"
+  end
+
+  # Register a keyboard shortcut for a given url. When the shortcut key is pressed,
+  # the url is called remotely via an Ajax call.
+  def remote_shortcut_fnkey key, url, options={:method=>:get}
+   shortcut_fnkey_function key, remote_function(options.merge(:url=>url))
   end
 
   # Used to jump to locations using Fnkey
